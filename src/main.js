0bde55c77c6d4ff31a19eb8e4b1e45264a36c496
Gabor Javorszky <gabor@javorszky.co.uk>
2021-09-13 08:58:33 +0100

    Add installation of supabase into vue app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Import supabase
+import supabase from 'vue-3-supabase'
+
+const app = createApp(App)
+
+// Use supabase
+app.use(supabase, {
+    supabaseUrl: import.meta.env.VITE_SUPABASE_URL,
+    supabaseKey: import.meta.env.VITE_SUPABASE_ANON_KEY,
+    options: {}
+})
+
+app.mount('#app')
